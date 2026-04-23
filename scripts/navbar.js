24272cb9af1c3be8ae5bc01df665c463b092dbe0
Jada Brown <jbrowbb@siue.edu>
2026-04-23 18:35:10 -0500

    link pop out

---
+++ scripts/navbar.js
@@ -0,0 +1,6 @@
+const navEl = document.querySelector('.nav');
+const hamburgerEl = document.querySelector('.hamburger');
+
+hamburgerEl.addEventListener('click', () => {
+    navEl.classList.add('nav--open');
+});
