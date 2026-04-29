7754b9d79398f7551535af1cd8d2c1c3fdcf4aae
Jada Brown <jbrowbb@siue.edu>
2026-04-29 14:03:00 -0500

    fix bars

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -3,4 +3,5 @@ const hamburgerEl = document.querySelector('.hamburger');
 
 hamburgerEl.addEventListener('click', () => {
     navEl.classList.toggle('nav--open');
+    hamburgerEl.classList.toggle('hamburger--open');
 });
